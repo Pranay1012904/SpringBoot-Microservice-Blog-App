5dbd395d8f088dbf8e1a321811ca29cbf9fd6445
PranayXOX <Pranay.dmnsns@gmail.com>
2024-04-14 15:50:03 +0530

    Added App Constants

--- src/main/java/com/blog/app/one/controller/PostController.java
+++ src/main/java/com/blog/app/one/controller/PostController.java
@@ -4,6 +4,7 @@ import com.blog.app.one.dto.PostDto;
 import com.blog.app.one.dto.PostPaginationResponse;
 import com.blog.app.one.entity.Post;
 import com.blog.app.one.service.PostService;
+import com.blog.app.one.utils.Constants;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +24,9 @@ public class PostController {
     }
     @GetMapping("/get/allPosts")
     public ResponseEntity<PostPaginationResponse> getAllPost(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value="pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy
+            @RequestParam(value = Constants.PAGE_NUMBER, defaultValue = Constants.PAGE_DEFAULT_VALUE, required = false) int pageNo,
+            @RequestParam(value= Constants.PAGE_SIZE, defaultValue = Constants.PAGE_DEFAULT_ELEMENTS, required = false) int pageSize,
+            @RequestParam(value = Constants.SORT_BY, defaultValue = Constants.SORT_DEFAULT_VALUE, required = false) String sortBy
     ){
         PostPaginationResponse allPosts= postService.getAllPosts(pageNo, pageSize, sortBy);
        return new ResponseEntity<>(allPosts,HttpStatus.OK);
---
+++ src/main/java/com/blog/app/one/utils/Constants.java
@@ -0,0 +1,11 @@
+package com.blog.app.one.utils;
+
+public class Constants {
+
+    public static final String PAGE_NUMBER="pageNo";
+    public static final String PAGE_DEFAULT_VALUE="0";
+    public static final String PAGE_SIZE="pageSize";
+    public static final String PAGE_DEFAULT_ELEMENTS="10";
+    public static final String SORT_BY="sortBy";
+    public static final String SORT_DEFAULT_VALUE="id";
+}
